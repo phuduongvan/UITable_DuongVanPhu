e6e893a9e6ccdf2858e222c7bbf107eff9859a93
Cntt21 <Admin21@Cntt21s-iMac.local>
2017-04-26 15:56:20 +0700

    Initial Commit

---
+++ UITableView3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITableView3
+//
+//  Created by Cntt21 on 4/26/17.
+//  Copyright © 2017 Cntt21. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
